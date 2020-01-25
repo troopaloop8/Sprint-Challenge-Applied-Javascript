0f999be3864bca86211194d2ad9627a9e8dc108b
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-01-25 11:55:13 +0900

    cards done!

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    Object.keys(response.data.articles).forEach(type => {
+      response.data.articles[type].forEach(text => {
+        cardContainer.appendChild(cardCreator(text));
+      });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function cardCreator(data) {
+    
+  const newCard = document.createElement("div");
+  const newHeadline = document.createElement("div");
+  const newAuthor = document.createElement("div");
+  const newImgContainer = document.createElement("div");
+  const newImg = document.createElement("img");
+  const newCredit = document.createElement("span");
+
+  newCard.classList.add("card");
+  newHeadline.classList.add("headline");
+  newAuthor.classList.add("author");
+  newImgContainer.classList.add("img-container");
+
+  newHeadline.textContent = data.headline;
+  newImg.src = data.authorPhoto;
+  newCredit.textContent = `By ${data.authorName}`;
+
+  newCard.appendChild(newHeadline);
+  newCard.appendChild(newAuthor);
+  newAuthor.appendChild(newImgContainer);
+  newImgContainer.appendChild(newImg);
+  newAuthor.appendChild(newCredit);
+
+  return newCard;
+}
